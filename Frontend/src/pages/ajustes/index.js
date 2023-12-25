a0fbc510654248c9bab514ce0d52b282cba31dc9
andrealopezco20 <alopezco@unsa.edu.pe>
2023-12-25 11:29:53 -0500

    Cambio de index ajustes

--- Frontend/src/pages/ajustes/index.js
+++ Frontend/src/pages/ajustes/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import { Button } from "reactstrap";
 import apiMykonos from "services/apiMykonos";
-import { useEffect } from "react";
+import { useEffect, React, useState } from "react";
 import { useSnackbar } from "contexts/SnackbarContext";
 import { Container, Typography } from "@mui/material";
 
