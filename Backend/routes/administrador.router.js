23854743eb54dc54faa7808cc4d3546d8bfedf16
Piero Vizcarra Vargas <77543520+Pev40@users.noreply.github.com>
2023-12-25 19:41:14 -0500

    Proyecto Funcional

--- Backend/routes/administrador.router.js
+++ Backend/routes/administrador.router.js
@@ -1,4 +1,4 @@
-var express = require('express');
+const express = require('express');
 const router = express.Router();
 /**
  * Documentación Completa
@@ -70,7 +70,7 @@ router.get('/buscarIDPorDNI',async(req,res)=>{
 
 
 router.put('/password',async(req,res)=>{
-  const { Password,PasswordNueva, DNI } = req.body;
+  const { PasswordNueva, DNI } = req.body;
   const info = await administradorDb.actualizarPassword(PasswordNueva,PasswordNueva,DNI);
   res.setHeader("Content-Type", "application/json");
   if (info.error) {
@@ -84,7 +84,6 @@ router.put('/password',async(req,res)=>{
 
 
 router.delete('/eliminar',async(req,res)=>{
-  const DNI = req.query;
   const info = await administradorDb.deleteAdministrador(req.query.DNI);
   res.setHeader("Content-Type", "application/json");
   if (info == null) {
